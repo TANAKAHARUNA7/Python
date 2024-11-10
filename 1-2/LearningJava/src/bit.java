133c3930bca501590b672b4665b5253798495e47
haruna <haruna0306@g.yju.ac.kr>
2024-11-10 20:47:56 +0900

    python 정리

--- 1-2/LearningJava/src/bit.java
+++
@@ -1,9 +0,0 @@
-public class bit {
-    public static void main(String[] args) {
-        int a = 0b00000000001111111111000000000010;
-        int not = ~a;
-        String binaryString = Integer.toBinaryString(not);
-        System.out.println(binaryString);
-
-    }
-}
