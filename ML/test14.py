84b1f73d2242d67a879a7749648c7d7404b63dd1
haruna <haruna0306@g.yju.ac.kr>
2025-04-11 22:16:36 +0900

    linear regression

--- ML/test14.py
+++ ML/test14.py
@@ -1,15 +1,17 @@
 import numpy as np
-# H(x) = 5X + 3X = 3
+# H(x) = 5X1 + 3X2 + 3
 num_of_samples = 5
 num_of_features = 2
 
 # data set
 np.random.seed(42)
 np.set_printoptions(False, suppress=True)
+
+# np.random.rand(行数, 列数) -> 0〜1の間のランダムな小数を作る
 X = np.random.rand(num_of_samples , num_of_features) * 10
 x_true = [5, 3]
 b_true = 4
-noise = np.random.rand(num_of_samples) * 0.5
+noise = np.random.randn(num_of_samples) * 0.5
 y = X[:,0] * 5 + X[:, 1] * 3 + b_true + noise
 
 print(X)
