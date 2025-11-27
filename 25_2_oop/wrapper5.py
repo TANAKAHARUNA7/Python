a8a34ff3bc3d7de6b7a2b033cc609adcada0b646
haruna <haruna0306@g.yju.ac.kr>
2025-11-27 20:32:50 +0900

    flask

--- 25_2_oop/wrapper5.py
+++ 25_2_oop/wrapper5.py
@@ -1,14 +1,14 @@
 from functools import wraps
 
-def test(n):
+def test(path):
     def factory(func):
         def wrapper(func):
             func()
-    
         return wrapper
+    
     return factory
 
-@test(1)
+@test(1) # test(1) -> factory()(bar)
 def bar():
     ...
 
